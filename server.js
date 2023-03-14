e25ad73f8398cce593e3d77004fa59d84620e97b
CWJNor <124681318+CWJNor@users.noreply.github.com>
2023-03-14 12:18:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
- Access-Control-Allow-Origin: *
+
 const express=require('express');
+const cors = require('cors');
 const app=express();
 const port=8383;
-
+app.use(cors());
 app.use(express.static('public'))
 
 app.listen(port,()=>console.log('Server has started on port:$(port)'));
